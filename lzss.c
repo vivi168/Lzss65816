688a6e05d20dac53a2d3ec2ec55b4a788d667a87
Vivien B <vivienbihl@gmail.com>
2019-11-22 22:29:39 +0100

    make code more readable

--- lzss.c
+++ lzss.c
@@ -132,22 +132,40 @@ int getbit(int n) /* get n bits */
 
 void decode(void)
 {
-    int i, j, k, r, c;
-    
-    for (i = 0; i < N - F; i++) buffer[i] = ' ';
+    int byte, i, j, k, r, c;
+
     r = N - F;
+    
+    for (i = 0; i < r; i++) {
+        buffer[i] = ' ';
+    }
+
     while ((c = getbit(1)) != EOF) {
         if (c) {
-            if ((c = getbit(8)) == EOF) break;
-            fputc(c, outfile);
-            buffer[r++] = c;  r &= (N - 1);
+            byte = getbit(8);
+
+            if (byte == EOF) {
+                break;
+            }
+
+            fputc(byte, outfile);
+            buffer[r] = byte;
+            r += 1;
+            r &= (N - 1);
         } else {
-            if ((i = getbit(EI)) == EOF) break;
-            if ((j = getbit(EJ)) == EOF) break;
+            i = getbit(EI);
+            j = getbit(EJ);
+
+            if (i == EOF || j == EOF) {
+                break;
+            }
+
             for (k = 0; k <= j + 1; k++) {
                 c = buffer[(i + k) & (N - 1)];
                 fputc(c, outfile);
-                buffer[r++] = c;  r &= (N - 1);
+                buffer[r] = c;
+                r += 1;
+                r &= (N - 1);
             }
         }
     }
